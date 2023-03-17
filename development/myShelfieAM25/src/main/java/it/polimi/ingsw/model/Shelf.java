d2d76f252e286a88e819abeaa924cde5d8ff66e4
Marco Alfano <marco2.alfano@mail.polimi.it>
2023-03-17 18:27:04 +0100

    Shelf class signatures

--- development/myShelfieAM25/src/main/java/it/polimi/ingsw/model/Shelf.java
+++ development/myShelfieAM25/src/main/java/it/polimi/ingsw/model/Shelf.java
@@ -1,4 +1,30 @@
 package it.polimi.ingsw.model;
 
 public class Shelf {
+
+    public Shelf() {
+        this.shelf = new TilesType[yBound][xBound];
+    }
+
+    /*
+     * adds a list of Tiles to the shelf
+     * @param    tiles   List of tiletype, ordered by order of insertion
+     * @param    column  int, destination column
+     * */
+    public void add(List<TileType> tiles, int column);
+
+    /*
+    * returns calculated score of the shelf
+    * */
+    int calculatePoints();
+
+    /*
+     * Getter for TyleType of a single slot on the shelf, returns null in case it is blank
+     * */
+    TypeType getTile(int x, int y);
+
+    /*
+     * Getter for array representing current state of shelf
+     * */
+    public TilesType[][] getShelf();
 }
