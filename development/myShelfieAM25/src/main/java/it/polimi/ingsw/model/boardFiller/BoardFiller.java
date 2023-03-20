29ece98a0ff39a2fcf7ff35577fe1fc8cb08c2f1
andreac01 <andrea2.cerutti@mail.polimi.it>
2023-03-20 22:28:21 +0100

    fixed management of empty bag in BoardFiller, updated javadoc

--- development/myShelfieAM25/src/main/java/it/polimi/ingsw/model/boardFiller/BoardFiller.java
+++ development/myShelfieAM25/src/main/java/it/polimi/ingsw/model/boardFiller/BoardFiller.java
@@ -5,33 +5,36 @@ import it.polimi.ingsw.model.TilesType;
 import java.util.Random;
 
 import it.polimi.ingsw.model.exception.*;
+
 /**
- * Creating a class that represent an element that is able to randomly refill the game board when requested
+ * BoardFiller is the abstract class whose subclasses are responsible for refilling the game board with random tiles
+ * when ever it is requested by the LivingRoom
  * In order to implement this functionality we'll use a strategy pattern that incrementally fill the board based on the subtype of filler used
  * @author ceru
  */
 public abstract class BoardFiller {
     /**
-     * remainingTiles represents the constraints on the limited number of tiles existing for each TileType
-     * the attribute is static because it is used in all subclasses
+     * remainingTiles represents the constraints on the number of tiles for each TilesType that are present in the bag
+     * the attribute is protected because it is used and accessed in all subclasses
      */
     protected int[] remainingTiles;
 
     /**
-     * random is a random number generator used for extracting random TileType
+     * random is a random number generator used for extracting random TilesType
      */
     protected Random random;
 
     /**
-     * public interface of the method used to refill the board
-     * @return TilesType[][]    it returns a new filled 9x9 game board
+     * public signature of the method used to refill the board, this is the abstract method that subclasses have to implement
+     * @return TilesType[][]    it returns a new filled (if possible) 9x9 game board
      */
-    public abstract TilesType[][] fill(TilesType[][] board) throws NoTileLeftException;
+    public abstract TilesType[][] fill(TilesType[][] board);
 
     /**
-     * protected function used for getting a random tile type to place on the board
-     * @return  index of the tile
-     * @throws NoTileLeftException  if the number of generated tile exceeds the number of tile in the game
+     * Protected function used for getting a random tile type to place on the board
+     * This function guarantees that when a int is returned there where tiles remaining of that type
+     * @return  index of the tile to use
+     * @throws NoTileLeftException if the number of generated tile exceeds the number of tile in the game
      */
     protected int getNextType() throws NoTileLeftException{
         int curType;
@@ -45,10 +48,10 @@ public abstract class BoardFiller {
     }
 
     /**
-     * private method used to check if no tiles are available to be placed
-     * @return true iff there are tiles available
+     * private method used to check if there is any tile available to be placed
+     * @return true only if there are tiles available
      */
-    private boolean checkRemaining(){
+    public boolean checkRemaining(){
         for(int i=0; i<6; i++){
             if (remainingTiles[i]>0){
                 return true;
--- development/myShelfieAM25/src/main/java/it/polimi/ingsw/model/boardFiller/TwoPlayerFiller.java
+++ development/myShelfieAM25/src/main/java/it/polimi/ingsw/model/boardFiller/TwoPlayerFiller.java
@@ -6,19 +6,30 @@ import java.util.Random;
 
 import it.polimi.ingsw.model.exception.*;
 
+/**
+ * TwoPlayerFiller is the concrete implementation of a BoardFiller responsible for refilling a game board for a two-player game
+ * @author ceru
+ */
 public class TwoPlayerFiller extends BoardFiller{
+
     /**
      * The constructor of the class initialize an array with the number of remaining tiles
-     * The initialization with the current system time allows further randomization
+     * The initialization of random using the current system time allows further randomization
      */
     public TwoPlayerFiller(){
         this.remainingTiles = new int[]{22,22,22,22,22,22};
         this.random = new Random(System.currentTimeMillis());
     }
-    public TilesType[][] fill(TilesType[][] board) throws NoTileLeftException{
+
+    /**
+     * implementation of the fill() method requested by extending the BoardFiller abstract class
+     * @return TilesType[][]    it returns a new filled (if possible) 9x9 game board as requested in a two-player type of game
+     */
+    public TilesType[][] fill(TilesType[][] board){
         int i;
 
-        if(board[1][3]==null) board[1][3] = TilesType.values()[getNextType()];
+        try {
+            if(board[1][3]==null) board[1][3] = TilesType.values()[getNextType()];
 
         if(board[1][4]==null) board[1][4] = TilesType.values()[getNextType()];
 
@@ -46,5 +57,8 @@ public class TwoPlayerFiller extends BoardFiller{
         if(board[7][5]==null) board[7][5] = TilesType.values()[getNextType()];
 
         return board;
+        } catch (NoTileLeftException e) {
+            return board;
+        }
     }
 }
