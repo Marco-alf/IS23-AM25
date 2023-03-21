7d6b88f0bb015965ab15c7e6f8ad5eca3af5dfbd
andreac01 <andrea2.cerutti@mail.polimi.it>
2023-03-21 12:12:25 +0100

    added signatures for Player and Game

--- development/myShelfieAM25/src/main/java/it/polimi/ingsw/model/Game.java
+++ development/myShelfieAM25/src/main/java/it/polimi/ingsw/model/Game.java
@@ -1,10 +1,42 @@
 package it.polimi.ingsw.model;
 
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+/**
+ * TO DO: model of the class Game, ALL methods have to be implemented
+ */
 public class Game
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    private ArrayList<Player> players;
+    private ArrayList<Player> onlinePlayers;
+    private LivingRoom board;
+    private Player currentPlayer;
+    
+
+    public Game(ArrayList<String> players) {
     }
+
+    public ArrayList<Integer> getPlayersPoints() {
+        return new ArrayList<Integer>();
+    }
+
+    public Player updateCurrentPlayer(){
+        return players.get(0);
+    }
+
+    public void moveTiles(ArrayList<Tile> tiles, int shelfColumn, String player){
+
+    }
+
+    public ArrayList<String> getOnlinePlayers(){
+        ArrayList<String> online = new ArrayList<String>();
+        return online;
+    }
+
+    public void disconnectPlayer(String player){
+
+    }
+
 }
--- development/myShelfieAM25/src/main/java/it/polimi/ingsw/model/Player.java
+++ development/myShelfieAM25/src/main/java/it/polimi/ingsw/model/Player.java
@@ -1,4 +1,33 @@
 package it.polimi.ingsw.model;
 
+import java.util.ArrayList;
+
+/**
+ * TO DO: model of the class Player, ALL methods have to be implemented
+ */
 public class Player {
+    private String name;
+    private Shelf shelf;
+    private LivingRoom board;
+
+    public int calculatePersonalPoints(){
+        return 0;
+    }
+
+    public int calculateCommonPoints(){
+        return 0;
+    }
+
+    public TilesType[][] getShelf(){
+        TilesType[][] tilesTypes = new TilesType[][]{};
+        return tilesTypes;
+    }
+
+    public void moveTiles(ArrayList<Tile> tiles, int shelfColumn){
+
+    }
+
+    public int calculateAdjacentPoints(){
+        return 0;
+    }
 }
